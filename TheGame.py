9a975963df466c8460c0333e1cc0b2c2d8674153
homer1013 <homer1013@gmail.com>
2024-08-29 11:38:57 -0600

    First Push
    Creating a trivia game to practice what I'm learning

---
+++ TheGame.py
@@ -0,0 +1,23 @@
+print("Hello! Welcome to The Game\n\n")
+
+user_name = input("What is your name?\n")
+
+print("\nThanks for choosing to play today, " + user_name + "\n")
+
+answer = int(input("Question 1. When did the Roman Empire fall?\n"))
+
+if answer == 476:
+	print("That's Correct!")
+else:
+	print("I'm sorry, that's not correct")
+
+thing = input()
+
+answer = input("Question 2. What is the two letter symbol for the element Gold?\n")
+
+if answer == "Au" or "AU" or "au":
+	print("That's Correct!")
+else:
+	print("I'm sorry, that's not correct")
+
+thing = input()
